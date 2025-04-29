a81c9652e2353717711973ee12af4fc4547ef898
Your Name <you@example.com>
2025-04-29 16:08:08 +0300

    updates

--- kappabay-new-ui/app/providers.tsx
+++ kappabay-new-ui/app/providers.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import { FC } from "react";
-/* import {
-  AllDefaultWallets,
-  defineStashedWallet,
-  ,
-} from "@suiet/wallet-kit"; */
 import React from "react";
 import { isEnokiNetwork, registerEnokiWallets } from "@mysten/enoki";
 import { useSuiClientContext, createNetworkConfig, SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { useEffect } from "react";
-import { getFullnodeUrl, type SuiClientOptions } from "@mysten/sui/client";
+import { getFullnodeUrl } from "@mysten/sui/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { EnokiFlowProvider } from "@mysten/enoki/react";
 
 // Config options for the networks you want to connect to
 const { networkConfig } = createNetworkConfig({
@@ -27,25 +20,25 @@ const queryClient = new QueryClient();
  * @constructor
  */
 function RegisterEnokiWallets() {
-  const { client, network } = useSuiClientContext();
+	const { client, network } = useSuiClientContext();
 
-  useEffect(() => {
-    if (!isEnokiNetwork(network)) return;
+	useEffect(() => {
+		if (!isEnokiNetwork(network)) return;
 
-    const { unregister } = registerEnokiWallets({
-      apiKey: process.env.NEXT_PUBLIC_ENOKI_API_KEY!,
-      providers: {
-        google: { clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID! },
-        // facebook: { clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID! },
-      },
-      client,
-      network,
-    });
+		const { unregister } = registerEnokiWallets({
+			apiKey: process.env.NEXT_PUBLIC_ENOKI_API_KEY!,
+			providers: {
+				google: { clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID! },
+				// facebook: { clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID! },
+			},
+			client,
+			network,
+		});
 
-    return unregister;
-  }, [client, network]);
+		return unregister;
+	}, [client, network]);
 
-  return null;
+	return null;
 }
 interface ProvidersProps {
 	children: React.ReactNode;
@@ -53,17 +46,15 @@ interface ProvidersProps {
 
 const Providers: React.FC<ProvidersProps> = ({ children }) => {
 	return (
-		<EnokiFlowProvider apiKey={process.env.NEXT_PUBLIC_ENOKI_API_KEY ?? ""}>
-			<QueryClientProvider client={queryClient}>
-				<SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-					<RegisterEnokiWallets />
-					<WalletProvider autoConnect={true}>
-						{children}
-						<Toaster />
-					</WalletProvider>
-				</SuiClientProvider>
-			</QueryClientProvider>
-		</EnokiFlowProvider>
+		<QueryClientProvider client={queryClient}>
+			<SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+				<RegisterEnokiWallets />
+				<WalletProvider autoConnect={true}>
+					{children}
+					<Toaster />
+				</WalletProvider>
+			</SuiClientProvider>
+		</QueryClientProvider>
 	);
 };
 
